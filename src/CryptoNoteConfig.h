bdf13a4a18de0cab21ebdd9ce0c02bad921f03b1
Suzyo Nyirenda <suzyoeliasnyirenda@gmail.com>
2019-02-22 22:09:47 +0200

    changed address prefix

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -16,7 +16,7 @@ namespace parameters {
 const uint64_t CRYPTONOTE_MAX_BLOCK_NUMBER													= 500000000;
 const size_t   CRYPTONOTE_MAX_BLOCK_BLOB_SIZE												= 500000000;
 const size_t   CRYPTONOTE_MAX_TX_SIZE														= 1000000000;
-const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX										= 0x78c9; /* anx address prefix */
+const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX										= 0x641c7; /* anx address prefix */
 const size_t   CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW											= 10; /* 20 minutes */
 const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT											= 60 * 60 * 2; /* two hours */
 const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT_V1										= 360; /* changed for LWMA3 */
