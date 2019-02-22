c313cd4ffa148037b7f55d6366aea12b74e09f33
Aluisyo <suzyoeliasnyirenda@gmail.com>
2019-02-22 21:22:06 +0200

    Fork from Conceal Network to Aluisyo Network

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2011-2017 The Cryptonote developers
+// Copyright (c) 2018 The Circle Foundation
+// Copyright (c) 2019 Aluisyo network
+//
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+ const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0xFA, 0x01, 0xB1, 0xB2, 0xC5, 0x5A, 0x80, 0x75, 0xF5, 0x12, 0xA3, 0x36, 0xE5, 0xE3, 0xDD, 0x17 } };
+}
