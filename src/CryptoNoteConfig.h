89da2134b685546645b4dce504dd9292bc7f2863
Aluisyo <suzyoeliasnyirenda@gmail.com>
2019-02-25 16:54:56 +0200

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -116,14 +116,14 @@ const char     MINER_CONFIG_FILE_NAME[]                      								= "miner_co
 } // parameters
 
 const uint64_t START_BLOCK_REWARD															= (UINT64_C(5000) * parameters::POINT); // start reward 
-const uint64_t FOUNDATION_TRUST																= (UINT64_C(12000000) * parameters::COIN); // locked funds to secure network  
+const uint64_t FOUNDATION_TRUST																= (UINT64_C(0) * parameters::COIN); // locked funds to secure network  
 const uint64_t MAX_BLOCK_REWARD					= (UINT64_C(50) * parameters::COIN); // max reward
 const uint64_t REWARD_INCREASE_INTERVAL				= (UINT64_C(21900)); // aprox. 1 month (+ 0.25 ANX increment per month)
 
 const char     CRYPTONOTE_NAME[]                             	= "aluisyo";
-const char     GENESIS_COINBASE_TX_HEX[]			= "010a01ff0001c096b102029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101e773a3b6ea14721766ce692ad09280afe4057fd4a9203d2f256bfb1aedb89646";
+const char     GENESIS_COINBASE_TX_HEX[]			= "";
 const uint32_t GENESIS_NONCE                         	        = 10000;
-const uint64_t GENESIS_TIMESTAMP				= 1551013428;
+const uint64_t GENESIS_TIMESTAMP				= 1551106337;
 const uint8_t  TRANSACTION_VERSION_1				=  1;
 const uint8_t  TRANSACTION_VERSION_2				=  2;
 const uint8_t  BLOCK_MAJOR_VERSION_1				=  1; // (Consensus I)
@@ -161,7 +161,7 @@ const uint32_t P2P_DEFAULT_CONNECTION_TIMEOUT			= 5000; // 5 seconds
 const uint32_t P2P_DEFAULT_PING_CONNECTION_TIMEOUT		= 2000; // 2 seconds
 const uint64_t P2P_DEFAULT_INVOKE_TIMEOUT			= 60 * 2 * 1000; // 2 minutes
 const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT		= 5000; // 5 seconds
-const char     P2P_STAT_TRUSTED_PUB_KEY[]			= "s9206a9e0f0e30986afde49l5bfba921af60afdb44304673d460a9ea34bf5c5a";
+const char     P2P_STAT_TRUSTED_PUB_KEY[]			= "a9206a9e0l0e30986afde49lubiba02esf30afdb44304673h460a9sa34bf5s5p";
 
 // Seed Nodes
 const std::initializer_list<const char*> SEED_NODES = {
