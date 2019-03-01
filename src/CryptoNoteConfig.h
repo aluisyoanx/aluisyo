882e09aca85b7091c9c049a7b4e8d838b87f1372
Suzyo Nyirenda <suzyoeliasnyirenda@gmail.com>
2019-03-01 18:54:19 +0200

    update

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2011-2017 The Cryptonote Developers
 // Copyright (c) 2018 The Circle Foundation
-// Copyright (c) 2018 Aluisyo Network
+// Copyright (c) 2019 Aluisyo
 //
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
